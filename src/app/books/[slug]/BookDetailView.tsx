40f6b68c6b8f351699ccd1293fe49f84a77d6a20
tjdghks7373 <tjdghks7373@naver.com>
2026-03-26 21:30:55 +0900

    fix: 카카오 SDK integrity 제거

--- src/app/books/[slug]/BookDetailView.tsx
+++ src/app/books/[slug]/BookDetailView.tsx
@@ -240,8 +240,6 @@ export default function BookDetailView({ book }: { book: Book }) {
     <>
       <Script
         src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
-        integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4"
-        crossOrigin="anonymous"
         onLoad={() => {
           if (!window.Kakao.isInitialized()) {
             window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_APP_KEY!);
